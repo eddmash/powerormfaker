e89375d4007c76b8100f9e93b1edff97eba2d5dc
edd.cowan@gmail.com <edd.cowan@gmail.com>
2018-11-12 11:36:17 +0300

    update to use components

--- src/Commands/Generatedata.php
+++ src/Commands/Generatedata.php
@@ -12,10 +12,13 @@
 namespace Eddmash\PowerOrmFaker\Commands;
 
 use Eddmash\PowerOrm\BaseOrm;
+use Eddmash\PowerOrm\Components\AppInterface;
 use Eddmash\PowerOrm\Exception\CommandError;
+use Eddmash\PowerOrm\Exception\ComponentException;
 use Eddmash\PowerOrm\Model\Model;
 use Eddmash\PowerOrmFaker\Populator;
 use Faker\Factory;
+use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
@@ -29,6 +32,16 @@ class Generatedata extends Command
      */
     public function handle(InputInterface $input, OutputInterface $output)
     {
+        $app_name = $input->getArgument('app_name');
+        try {
+            $component = BaseOrm::getInstance()->getComponent($app_name);
+        } catch (ComponentException $e) {
+            throw new CommandError(sprintf("Could not find an application with the name %s", $app_name));
+        }
+        if (!$component instanceof AppInterface) {
+            throw new CommandError(sprintf("is not an application %s, it does not implement %s", $app_name,
+                AppInterface::class));
+        }
         try {
             $connection = BaseOrm::getDbConnection();
 
@@ -40,10 +53,24 @@ class Generatedata extends Command
             }
             $populator = new Populator($faker);
 
-            $only = $input->getOption('only');
+            $onlys = $input->getOption('only');
 
-            if ($only) {
-                $models = $this->getModels($only);
+            // assumes the app is using composer to autload using psr-4
+            $namespace = sprintf("%s\%s", $component->getNamespace(), substr($component->getModelsPath(),
+                strrpos($component->getModelsPath(), DIRECTORY_SEPARATOR) + 1));
+            $namespace = ltrim($namespace, "\\");
+
+            $classnames = [];
+            foreach ($onlys as $only) {
+                if (!strrpos($only, "\\")) {
+                    $classnames[] = sprintf("%s\%s", $namespace, $only);
+                } else {
+                    $classnames[] = $only;
+                }
+            }
+
+            if ($classnames) {
+                $models = $this->getModels($classnames);
             } else {
                 $models = BaseOrm::getRegistry()->getModels(true);
 
@@ -79,7 +106,7 @@ class Generatedata extends Command
 
             if ($ignore && $only) {
                 throw new CommandError(
-                    'Its not allowed to set both '.
+                    'Its not allowed to set both ' .
                     "'only' and 'ignore' options at the same time"
                 );
             }
@@ -103,37 +130,43 @@ class Generatedata extends Command
     protected function configure()
     {
         $this->setName($this->guessCommandName())
-             ->setDescription($this->help)
-             ->setHelp($this->help)
-             ->addOption(
-                 'seed',
-                 '-s',
-                 InputOption::VALUE_REQUIRED | InputOption::VALUE_IS_ARRAY,
-                 'Always the same generated data. '.
-                 'using the same seed produces the same results',
-                 null
-             )
-             ->addOption(
-                 'only',
-                 '-o',
-                 InputOption::VALUE_REQUIRED | InputOption::VALUE_IS_ARRAY,
-                 'The list of models to use when generating records.',
-                 null
-             )
-             ->addOption(
-                 'ignore',
-                 '-i',
-                 InputOption::VALUE_REQUIRED | InputOption::VALUE_IS_ARRAY,
-                 'The list of models to ignore when generating records.',
-                 null
-             )
-             ->addOption(
-                 'records',
-                 '-r',
-                 InputOption::VALUE_REQUIRED,
-                 'The number of records to generate per model.',
-                 null
-             );
+            ->setDescription($this->help)
+            ->setHelp($this->help)
+            ->addArgument(
+                'app_name',
+                InputArgument::REQUIRED,
+                'App for which to generate data for',
+                null
+            )
+            ->addOption(
+                'seed',
+                '-s',
+                InputOption::VALUE_REQUIRED | InputOption::VALUE_IS_ARRAY,
+                'Always the same generated data. ' .
+                'using the same seed produces the same results',
+                null
+            )
+            ->addOption(
+                'only',
+                '-o',
+                InputOption::VALUE_REQUIRED | InputOption::VALUE_IS_ARRAY,
+                'The list of models to use when generating records.',
+                null
+            )
+            ->addOption(
+                'ignore',
+                '-i',
+                InputOption::VALUE_REQUIRED | InputOption::VALUE_IS_ARRAY,
+                'The list of models to ignore when generating records.',
+                null
+            )
+            ->addOption(
+                'records',
+                '-r',
+                InputOption::VALUE_REQUIRED,
+                'The number of records to generate per model.',
+                null
+            );
     }
 
     public function getModels($names = [])
